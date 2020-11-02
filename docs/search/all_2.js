843f031b580fa4791e5c4f795a76a5915b924e23
Autofire <Autofire@users.noreply.github.com>
2020-11-01 19:52:58 -0800

    Re-added the Doxygen files.

---
+++ docs/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['charactercommands',['CharacterCommands',['../class_dialogue_1_1_v_n_1_1_character_commands.html',1,'Dialogue::VN']]],
+  ['charactercommands_2ecs',['CharacterCommands.cs',['../_character_commands_8cs.html',1,'']]]
+];
---
+++ docs/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['charactercommands_2ecs',['CharacterCommands.cs',['../_character_commands_8cs.html',1,'']]]
+];
