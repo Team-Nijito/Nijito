7ab24615b69aa4e34cf5dea57d92b8da13c2b104
Autofire <Autofire@users.noreply.github.com>
2020-12-14 14:52:39 -0800

    Created YarnScriptLoader.

---
+++ Assets/Scripts/UI/VN/YarnScriptLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnScriptLoader : MonoBehaviour
+{
+    public YarnProgram script;
+    public DialogueRunner runner;
+
+    void Start()
+    {
+        runner.Add(script);
+        //runner.StartDialogue(script.GetProgram().Name);
+        runner.StartDialogue();
+    }
+
+}
