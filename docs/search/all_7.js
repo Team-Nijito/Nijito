6ad34b75a5705cf68cfa461560ddbf1241169d21
Autofire <Autofire@users.noreply.github.com>
2020-12-14 15:08:29 -0800

    Rebuilt docs.

---
+++ docs/search/all_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['imagerenderer',['imageRenderer',['../class_dialogue_1_1_v_n_1_1_puppet.html#a97481d7ad130c09aaa7c7ca24450e2f9',1,'Dialogue::VN::Puppet']]],
+  ['initialfacing',['initialFacing',['../class_dialogue_1_1_v_n_1_1_puppet.html#a416b2a1ce1de1b9f67f05d8a58d2662f',1,'Dialogue::VN::Puppet']]],
+  ['itembox',['ItemBox',['../class_dialogue_1_1_v_n_1_1_stage_commands.html#a12d61cadf8539b751e781bd23984da41',1,'Dialogue::VN::StageCommands']]]
+];
---
+++ docs/search/variables_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['puppetprefabs',['puppetPrefabs',['../class_dialogue_1_1_v_n_1_1_puppet_master.html#a7c93322162792a19e9201fcc8235625e',1,'Dialogue::VN::PuppetMaster']]],
+  ['puppetspawnpoint',['puppetSpawnPoint',['../class_dialogue_1_1_v_n_1_1_puppet_master.html#a350718f6f83fe7dd955ef729d58d82f1',1,'Dialogue::VN::PuppetMaster']]]
+];
