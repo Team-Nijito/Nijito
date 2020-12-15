7585bbd21400e0fd79dbc0865dd7e7dda50aeb13
Autofire <Autofire@users.noreply.github.com>
2020-12-15 13:00:06 -0800

    Loader now correctly loads scripts.

--- Assets/Scripts/UI/VN/YarnScriptLoader.cs
+++ Assets/Scripts/UI/VN/YarnScriptLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
@@ -10,9 +11,10 @@ public class YarnScriptLoader : MonoBehaviour
 
     void Start()
     {
+        Debug.Log(script.GetProgram().Nodes.First().Key);
         runner.Add(script);
         //runner.StartDialogue(script.GetProgram().Name);
-        runner.StartDialogue();
+        runner.StartDialogue(script.GetProgram().Nodes.First().Key);
     }
 
 }
