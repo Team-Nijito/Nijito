df7a6024a7ad275c42ad53bfa9c77c266a613e0e
Autofire <Autofire@users.noreply.github.com>
2020-12-17 18:34:01 -0800

    Puppets now stack somewhat correctly.

--- Assets/Scripts/textHandling/VisualNovel/StagePoint.cs
+++ Assets/Scripts/textHandling/VisualNovel/StagePoint.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 namespace Dialogue.VN {
 	public class StagePoint : MonoBehaviour {
 
+		public float width;
+		
 		private List<GameObject> inhabitants;
 		private RectTransform rt;
 
@@ -13,26 +15,66 @@ namespace Dialogue.VN {
 			rt = GetComponent<RectTransform>();
 		}
 
-		public void AddInhabitant(GameObject obj) {
+		/// <summary>
+		/// Begins to assume that the given object will be residing at this point.
+		/// 
+		/// If the object is already an inhabitant, the order of the objects may change.
+		/// </summary>
+		/// <param name="obj">Objects to be registered.</param>
+		/// <param name="objPosition">The object's pivot position.</param>
+		public void AddInhabitant(GameObject obj, Vector2 objPosition) {
 			inhabitants.Remove(obj);
+
+			// TODO account for source's position.
 			inhabitants.Add(obj);
 		}
 
+		/// <summary>
+		/// Stop assuming that the given object wishes to reside at this point.
+		/// 
+		/// Nothing bad happens if object is not registered.
+		/// </summary>
+		/// <param name="obj">Registered object.</param>
 		public void RemoveInhabitant(GameObject obj) {
 			inhabitants.Remove(obj);
 		}
 
+		/// <summary>
+		/// Gets the position which has been assigned to the given object.
+		/// 
+		/// Object should be registered as an inhabitant of the point before calling this.
+		/// </summary>
+		/// <param name="obj">Objects requesting a position.</param>
+		/// <returns>The assigned position, or the centerpoint if the object is not been registered.</returns>
 		public Vector2 GetPosition(GameObject obj) {
-			Vector2 result = Position;
+			Vector2 result;
+			int index = inhabitants.IndexOf(obj);
 
-			if(inhabitants.Contains(obj)) {
+			if (index == -1) {
 				Debug.LogWarning(obj.name + " is requesting a position, but is not registered");
+				result = Center;
+			}
+			else {
+				float slice = width / inhabitants.Count;
+				float offset = slice * (index + 0.5f);
+				result = StartPoint + new Vector2(offset, 0);
 			}
 
 			return result;
 		}
-		public Vector2 Position {
+
+		/// <summary>
+		/// The center point. This never changes.
+		/// </summary>
+		public Vector2 Center {
 			get => (rt.anchorMax + rt.anchorMin) / 2;
 		}
+
+		private Vector2 StartPoint {
+			get => Center - new Vector2(width / 2, 0);
+		}
+
+
+
 	}
 }
