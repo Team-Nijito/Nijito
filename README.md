d980d3d228d25fc90e7168f37930bde8bd63a3fb
Autofire <Autofire@users.noreply.github.com>
2021-01-13 19:41:22 -0800

    Fixed typo. Added link to YT vids

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Nijito
 Documentation can be viewed here: https://team-nijito.github.io/Nijito/
+Current video guides are in this [Youtube playlist](https://youtube.com/playlist?list=PLP0-2ozLHYqm0sCnDsw0av7OIC4VGVxvx).
 
 Old version of the project is here: https://github.com/Esurielt/Nijito
 
--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -5,5 +5,5 @@ var searchData=
   ['alignment',['alignment',['../class_dialogue_1_1_v_n_1_1_stage_point.html#a26c7b5ff956703552d179a34d3299b73',1,'Dialogue::VN::StagePoint']]],
   ['animate',['Animate',['../class_dialogue_1_1_v_n_1_1_character_commands.html#af8f994ae298509f4907506b93a331afc',1,'Dialogue::VN::CharacterCommands']]],
   ['animatestage',['AnimateStage',['../class_dialogue_1_1_v_n_1_1_stage_commands.html#a8ff7caa15e0a21834fc6b5c33767aa4c',1,'Dialogue::VN::StageCommands']]],
-  ['animationcontroller',['animationController',['../class_dialogue_1_1_v_n_1_1_puppet.html#a27fd9a747122fd6d5e864336f1ebbc1a',1,'Dialogue::VN::Puppet']]]
+  ['animator',['animator',['../class_dialogue_1_1_v_n_1_1_puppet.html#a5933c0940cf43704fb7eb920f7abc9bd',1,'Dialogue::VN::Puppet']]]
 ];
