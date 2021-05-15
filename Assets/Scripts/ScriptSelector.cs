534d6e02c3dc6a2ab1a28e42ddaa093e831ad058
Autofire <Autofire@users.noreply.github.com>
2021-05-15 15:39:10 -0700

    Made it so that the runtime-selected scene takes precidence.

--- Assets/Scripts/ScriptSelector.cs
+++ Assets/Scripts/ScriptSelector.cs
@@ -8,6 +8,10 @@ namespace Dialogue {
 		private const string YarnPath = "Yarns/";
 		public static YarnProgram selectedYP { get; set; }
 
+		[System.Obsolete(
+			"String-based references are risky and shouldn't be used without good reason." +
+			"If you do have a good reason, remove the Obsolete attribute."
+		)]
 		public static void FromName(string ypName) {
 			selectedYP = Resources.Load(YarnPath + ypName) as YarnProgram;
 		}
--- Assets/Scripts/UI/VN/YarnScriptLoader.cs
+++ Assets/Scripts/UI/VN/YarnScriptLoader.cs
@@ -6,13 +6,13 @@ using Yarn.Unity;
 
 namespace Dialogue.VN {
 	public class YarnScriptLoader : MonoBehaviour {
-		public YarnProgram overrideScript;
+		public YarnProgram defaultScript;
 		public DialogueRunner runner;
 
 		void Start() {
 			//ScriptSelector.FromName("TestScript"); // TODO Remove
 
-			YarnProgram script = overrideScript ?? ScriptSelector.selectedYP;
+			YarnProgram script = ScriptSelector.selectedYP ?? defaultScript;
 
 			runner.Add(script);
 			runner.StartDialogue(script.GetProgram().Nodes.First().Key);
